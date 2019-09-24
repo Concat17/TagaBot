71774317c5a9b13692a6bc46eb04340550199db3
Demel <demel654@gmail.com>
2019-09-24 22:28:13 +0300

    Refactoring #1

--- main.go
+++ main.go
@@ -6,53 +6,92 @@ import (
 	"log"
 	"strings"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	tgbot "github.com/go-telegram-bot-api/telegram-bot-api"
 	//"TagaBot/database"
 )
 
+type messager struct {
+	bot    *tgbot.BotAPI
+	chatID int64
+}
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI("977213939:AAFbg20C4R3Avg9KhtWY2JrTTijncayLhX8")
+	bot := createBot()
+	u := startGetUpd()
+	updates, _ := bot.GetUpdatesChan(u)
+	monitoring(bot, updates)
+}
+
+func createBot() *tgbot.BotAPI {
+	bot, err := tgbot.NewBotAPI("977213939:AAFbg20C4R3Avg9KhtWY2JrTTijncayLhX8")
 	if err != nil {
 		log.Panic(err)
 	}
-
 	bot.Debug = true
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	return bot
+}
 
-	u := tgbotapi.NewUpdate(0)
+func startGetUpd() tgbot.UpdateConfig {
+	u := tgbot.NewUpdate(0)
 	u.Timeout = 60
+	return u
+}
 
-	updates, err := bot.GetUpdatesChan(u)
-
+func monitoring(bot *tgbot.BotAPI, updates tgbot.UpdatesChannel) {
+	var msg tgbot.MessageConfig
 	for update := range updates {
 		if update.Message == nil { // ignore any non-Message Updates
 			continue
 		}
 
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-
 		if update.Message.IsCommand() {
-
-			switch command := strings.Fields(update.Message.Text)[0]; command {
-			case "/greetings":
-				msg = tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Hello, %v!", update.Message.From.UserName))
-			case "/bye":
-				mes := fmt.Sprintf("Bye-bye! %v", update.Message.From.UserName)
-				msg = tgbotapi.NewMessage(update.Message.Chat.ID, mes)
-			default:
-				args := update.Message.CommandArguments()
-				sepArgs := strings.Split(args, " ")
-				database.MakeQuery(sepArgs[0], sepArgs[1], sepArgs[2], sepArgs[3])
-				//editArgs := strings.Join(sepArgs, "~")
-
-				//msg = tgbotapi.NewMessage(update.Message.Chat.ID, args)
-				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Article added")
-			}
-
+			msg = execCommnd(update)
+		} else {
+			msg = tgbot.NewMessage(update.Message.Chat.ID, update.Message.Text)
 		}
 
 		bot.Send(msg)
 	}
 }
+
+func execCommnd(update tgbot.Update) tgbot.MessageConfig {
+	var msg tgbot.MessageConfig
+
+	switch command := strings.Fields(update.Message.Text)[0]; command {
+	case "/greetings":
+		msg = greetings(update)
+	case "/bye":
+		msg = bye(update)
+	case "/test":
+		msg = addArticle(update)
+	default:
+
+	}
+
+	return msg
+}
+
+func greetings(update tgbot.Update) tgbot.MessageConfig {
+	msg := tgbot.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Hello, %v!", update.Message.From.UserName))
+	return msg
+}
+
+func bye(update tgbot.Update) tgbot.MessageConfig {
+	text := fmt.Sprintf("Bye-bye! %v", update.Message.From.UserName)
+	msg := tgbot.NewMessage(update.Message.Chat.ID, text)
+	return msg
+}
+
+func addArticle(update tgbot.Update) tgbot.MessageConfig {
+	args := update.Message.CommandArguments()
+	sepArgs := strings.Split(args, " ")
+	database.MakeQuery(sepArgs[0], sepArgs[1], sepArgs[2], sepArgs[3])
+	msg := tgbot.NewMessage(update.Message.Chat.ID, "Article added")
+	return msg
+}
+
+/*
+- func see all articles
+- func delete article
+- func see concrete article
+*/
